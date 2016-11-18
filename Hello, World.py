a5e0cfff0ccadda94ea3edcef6aa79779126dc5c
Elyinn Somarriba <esomarriba@jackhenry.com>
2016-11-18 13:55:32 -0600

    Initial Commit

--- Hello, World.py
+++ Hello, World.py
@@ -1,2 +1,3 @@
-name = raw_input('What is your name?\n')
-print 'Hi, %s.' % name
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print "iteration {iteration} is {name}".format(iteration=i, name=name)
