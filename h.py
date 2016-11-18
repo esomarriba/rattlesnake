22f9cb4a7518120934af94c57cc1562a9de1c352
esomarriba <esomarriba@jackhenry.com>
2016-11-18 14:17:06 -0600

    Delete h.py

--- h.py
+++
@@ -1,8 +0,0 @@
-friends = ['john', 'pat', 'gary', 'michael']
-for i, name in enumerate(friends):
-    print "iteration {iteration} is {name}".format(iteration=i, name=name)
-
-
-# Test 1
-# Test 2 (Online)
-# Test 3 (Desktop)
